9724703d302e916599ea90642764a706c409b2aa
Geun-Hwan <rmsghks1026@gmail.com>
2025-05-11 17:22:45 +0900

    f

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Geun-Hwan.github.io",
+  base: "/Geun-Hwan.github.io/",
 
   plugins: [react()],
   server: { port: 3000, open: true },
